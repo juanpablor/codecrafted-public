cca983d3901c3b0f80b5e5d93cacdb2e3cda3ef6
juanpablor <juanpablor@users.noreply.github.com>
2025-01-14 03:42:09 +0000

    deploy: juanpablor/codecrafted.co@c214db90c88a8d87d491ad93b1a90245289f32b8

--- component---src-templates-blog-post-tsx-4dd60ddd21db2498030c.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkcorpo_code_crafted_studios=self.webpackChunkcorpo_code_crafted_studios||[]).push([[455],{8733:function(t,e,n){n.r(e);var o=n(4755),s=n(3714),a=n(5221),c=n(6540);const l=n(8526)[0];e.default=t=>{let{data:e}=t;const{t:n}=(0,o.useTranslation)(),u=e.postsJson;return u?c.createElement(s.default,{data:l},c.createElement(a.A,{title:u.title,date:u.date,content:u.content,slug:u.slug})):c.createElement("div",null,n("Post not found"))}}}]);
-//# sourceMappingURL=component---src-templates-blog-post-tsx-4dd60ddd21db2498030c.js.map
